783c672c86f1544f0348f7c7d8b89e0bbbd8f51f
NatasjaAndersen <31003506+NatasjaA@users.noreply.github.com>
2017-08-25 10:48:01 +0200

    Tilføjelse af BMI opgave

--- bmi_opgave/bmi.js
+++ bmi_opgave/bmi.js
@@ -18,31 +18,3 @@ function bmi (){
 
 }
 bmi();
-
-//--------------------------------------------
-
-// function bmicalculator() {
-//     var weightText = 170;
-//     var heightText = 70;
-//     var weight = parseInt(weightText.value);
-//     var weight = parseInt (heightText.value);
-//     height = height / 100;
-
-// }if(){
-//     var BMI = weightText/(heightText*heightText);
-//     var resultMsg ="";
-// }
-// else if (BMI <19){
-//     console.log(msg+"BMI score=" + BMI.toFixed(2) + "Underweight");
-// } else if(BMI >=19){
-//     console.log(msg+"BMI score=" + BMI.toFixed(2) + "Normal");
-// }else if(BMI >=25){
-//     console.log(msg+"BMI score=" + BMI.toFixed(2) + "Overweight");
-// }else if(BMI >=30){
-//     console.log(msg+"BMI score=" + BMI.toFixed(2) + "Huge Overweight");
-// }else if(BMI >=40){
-//     console.log(msg+"BMI score=" + BMI.toFixed(2) + "Giant");
-// }
-// console.log(resultMsg);
-
-
